53db8762e91747a366cef2842e6d4552ea2383b5
ntiKentucky <57523317+ntiKentucky@users.noreply.github.com>
2019-12-06 13:09:32 +0100

    update på sten sax påse

--- js/script.js
+++ js/script.js
@@ -5,5 +5,39 @@ const computerScore_span = document.getElementById("computer-score");
 const scoreBoard_div = document.querySelector(".score-board");
 const result_div = document.querySelector(".result");
 const rock_div = document.getElementById("r");
-const Paper_div = document.getElementById("p");
-const rock_div = document.getElementById("r");
+const paper_div = document.getElementById("p");
+const scissors_div = document.getElementById("s");
+
+function getComputerChoice() {
+    const choices = ['r', 'p', 's']
+    const randomNUmber = Math.floor(Math.random() * 3);
+    return choices [randomNUmber];
+}
+
+
+function game(userChoice) {
+    const computerChoice = getComputerChoice();
+    switch (userChoice + computerChoice) {
+        case "rs":
+        case "pr":
+        case "sp":
+            console.log("USER WIN!");
+           break;
+    }
+}
+
+function main() {
+rock_div.addEventListener('click', function() {
+    game("r")
+})
+
+paper_div.addEventListener('click', function() {
+    game("p");
+})
+
+scissors_div.addEventListener('click', function() {
+    game("s")
+})
+}
+
+main(); 
